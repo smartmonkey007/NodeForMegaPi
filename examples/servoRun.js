30f272862916ff97f4be35cbd18eb73e452e300e
indream <flashindream@gmail.com>
2016-03-22 17:26:01 +0800

    update servo port

--- examples/servoRun.js
+++ examples/servoRun.js
@@ -1,9 +1,10 @@
 var MegaPi = require("../megapi").MegaPi;
 var bot = new MegaPi(onStart);
-
+var port = 6;
+var slot = 1;
 var level = 1;
 function loop(){
-  bot.servoRun(6,0,level?100:40);
+  bot.servoRun(port,slot,level?100:40);
   level = 1-level;
 }
 function onStart(){
